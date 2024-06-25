925eeba14cb510967d33f3b603be2ced5ed57649
yongchane <202021113@sangmyung.kr>
2024-06-25 18:18:55 +0900

    실험내용
    이름 추가

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Loading = () => {
-  return <div className="loader">로딩</div>
-}
+  return <div className="loader">나는 용찬</div>;
+};
 
-export default Loading
+export default Loading;
