85ae98d7c206dbf44ce744528fe1298d86f8eb94
wongakim-99 <202021000@sangmyung.kr>
2024-06-26 18:55:27 +0900

    #5 - useState테스트
    간단한 숫자 덧셈 뺄셈으로 useState테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import "./scss/style.scss"
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
@@ -9,11 +9,20 @@ import { Provider } from "react-redux"
 // 리덕스에 비동기 작업도 실행시키기 위해 redux-thunk를 적용함
 // const store = createStore(rootReducer)
 
+//커밋 테스트
 const App = () => {
   console.log('Hello')  //GDSC COMMIT TEST  HELLO GDSC
+  const [value, setValue] = useState(0);
   return (
     // <Provider store={store}>
-    <RouterProvider router={router} />
+    <div>
+      <RouterProvider router={router} />
+      <p>
+        현재 카운터 값은 <b>{value}</b>
+        <button onClick={()=>setValue(value+1)}>+1</button>
+        <button onClick={()=>setValue(value-1)}>-1</button>
+      </p>
+    </div>
     // </Provider>
   )
 }
